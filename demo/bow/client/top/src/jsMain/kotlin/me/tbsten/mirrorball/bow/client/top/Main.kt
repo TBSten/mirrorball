f01ad1832796a7135a12f9e7fb831e4f49779d7e
TBSten <81161390+TBSten@users.noreply.github.com>
2024-10-11 04:30:27 +0900

    :demo:bowを削除

--- demo/bow/client/top/src/jsMain/kotlin/me/tbsten/mirrorball/bow/client/top/Main.kt
+++
@@ -1,7 +0,0 @@
-package me.tbsten.mirrorball.bow.client.top
-
-import kotlinx.browser.document
-
-fun main() {
-    document.getElementById("top-element")!!.innerHTML = "top-page"
-}
--- demo/bow/client/webpack.config.d/entries.js
+++
@@ -1,7 +0,0 @@
-
-// refs: https://webpack.js.org/guides/code-splitting/#entry-points
-//config.entry = {
-//    index: config.entry,
-//}
-
-
